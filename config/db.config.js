fe19df5f8d211c6c211fe3fa7e23b2a877282000
FebrianAditya <febrian.aditya@student.upi.edu>
2024-12-16 11:13:33 +0700

    change value config

---
+++ config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "username": "postgres",
+    "password": "ncrQeXNkTcecugorCKBbbFPUBLlshKJU",
+    "database": "railway",
+    "host": "postgres-production-363b.up.railway.app",
+    "dialect": "postgres",
+    "port": "5432",
+    "pool": {
+      "max": 5,
+      "min": 0,
+      "acquire": 30000,
+      "idle": 10000
+    }
+}
